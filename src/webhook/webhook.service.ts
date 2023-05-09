fb73a35b122d24f03f2a765ee69087faaefea784
joenzkc <jokichaa@gmail.com>
2023-05-09 20:12:34 +0800

    no message

--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -108,7 +108,7 @@ export class WebhookService {
       console.log(totalSpent);
       const remaining = event.budget - totalSpent;
       if (remaining < 0) {
-        reply += `Remaining: You have exceeded your budget 😒\n`;
+        reply += `Remaining: You have exceeded your budget by  😒\n`;
       } else {
         reply += `Remaining: $${remaining}`;
       }
